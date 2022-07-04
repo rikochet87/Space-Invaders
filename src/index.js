3d73b6bcb90988b131b3256974cd5f6ee16ca141
matias <matias3282@hotmail.com>
2022-07-04 19:34:27 -0300

    projectiles added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import { Player, c, canvas } from "./player";
+import { Projectile } from "./projectile";
 
 const player = new Player();
+const projectiles = [];
 const keys = {
   a: {
     pressed: false,
@@ -13,12 +15,21 @@ const keys = {
   },
 };
 
-export function animate() {
-  
+function animate() {
   requestAnimationFrame(animate);
   c.fillStyle = "black";
   c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
+  projectiles.forEach((projectile, index) => {
+    if (projectile.position.y + projectile.radius <= 0) {
+      setTimeout(()=>{
+        projectiles.splice(index, 1);
+      }, 0)
+      
+    } else {
+      projectile.update();
+    }
+  });
 
   if (keys.a.pressed && player.position.x >= 0) {
     player.velocity.x = -7;
@@ -35,36 +46,53 @@ export function animate() {
 }
 animate();
 
-addEventListener("keydown", ({ key }) => {
-  switch (key) {
-    case "a":
-      console.log("left");
-      keys.a.pressed = true;
-      break;
-    case "d":
-      console.log("right");
-      keys.d.pressed = true;
-      break;
-    case " ":
-      console.log("space");
-      break;
-  }
-});
+(function () {
+  addEventListener("keydown", ({ key }) => {
+    switch (key) {
+      case "a":
+        // console.log("left");
+        keys.a.pressed = true;
+        break;
+      case "d":
+        // console.log("right");
+        keys.d.pressed = true;
+        break;
+      case " ":
+        // console.log("space");
+        projectiles.push(
+          new Projectile({
+            position: {
+              x: player.position.x + player.width / 2,
+              y: player.position.y,
+            },
+            velocity: {
+              x: 0,
+              y: -12,
+            },
+          })
+        );
+        // console.log(projectiles);
+        break;
+    }
+  });
+})();
 
-addEventListener("keyup", ({ key }) => {
-  switch (key) {
-    case "a":
-      console.log("left");
-      keys.a.pressed = false;
-      break;
-    case "d":
-      console.log("right");
-      keys.d.pressed = false;
-      break;
-    case " ":
-      console.log("space");
-      break;
-  }
-});
+(function () {
+  addEventListener("keyup", ({ key }) => {
+    switch (key) {
+      case "a":
+        // console.log("left");
+        keys.a.pressed = false;
+        break;
+      case "d":
+        // console.log("right");
+        keys.d.pressed = false;
+        break;
+      case " ":
+        // console.log("space");
+        break;
+    }
+  });
+})();
 
-export {player}
+export { player };
